78ef2b1582f3f35fb9f237c39af04118ed42e716
Muhammad Samiullah <msami398@gmail.com>
2019-02-07 01:30:41 +0500

    Bootstrap4 and jobs are showing up

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    pub_date = models.DateTimeField()
+    blog_image = models.ImageField(upload_to='image/')
--- Jobs/views.py
+++ Jobs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+def homepage(request):
+    # fetch all the objects of the job
+    job = Job.objects
+    return render(request, 'Jobs/home.html', {'job': job})
